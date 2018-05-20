a7ad856d9d57e7d47a40bf4ae387d7d115ede7f5
dbanyay <banyaydani@gmail.com>
2018-05-20 22:58:46 +0200

    lot of changes

--- main.m
+++ main.m
@@ -1,9 +1,25 @@
 close all
+clear all
 
 
 %% Read audio files
 
 [Fs,clean1s,clean2s,babbles,nonstats,shapeds] = readAudioFiles();
+
+%% Framing
+
+windowSize = 33; % in samples, has to be odd
+
+overlap = 0.5; % for hanning window 50% is appropriate
+
+%input = babbles;
+input = ones(size(babbles));
+
+framesTime = windowing(input,windowSize,overlap);
+
+%% Apply transform
+framesFreq = fft(framesTime);
+
 
 %% Gain function
 
@@ -15,3 +31,23 @@ close all
 
 %% Speech PSD estimator
 
+
+%% Apply gain
+
+framesProcessedFreq = applyGain(framesFreq);
+
+
+%% Inverse transform
+
+framesProcessedTime = ifft(framesProcessedFreq);
+
+%% Overlap add
+
+output = overlapAdd(framesProcessedTime,windowSize, overlap);
+
+subplot(211)
+plot(input);
+title('Input')
+subplot(212)
+plot(output);
+title('Output')
--- readAudioFiles.m
+++ readAudioFiles.m
@@ -26,29 +26,29 @@ speechshapednoise = audioread('samples\Speech_shaped_noise.wav');
 
 %% Create shorter versions for faster processing
 
-clean1s = clean1(1:5*Fs); % 5 seconds
-clean2s = clean2(1:5*Fs); % 5 seconds
-babbles = babblenoise(1:5*Fs);
-nonstats = nonstatnoise(1:5*Fs);
-shapeds = speechshapednoise(1:5*Fs);
+clean1s = clean1(1:5*Fs)'; % 5 seconds
+clean2s = clean2(1:5*Fs)'; % 5 seconds
+babbles = babblenoise(1:5*Fs)';
+nonstats = nonstatnoise(1:5*Fs)';
+shapeds = speechshapednoise(1:5*Fs)';
 
 timeaxis = 1:5*Fs;
 timeaxis = timeaxis./Fs;
 
-subplot(511)
-plot(timeaxis,clean1s);
-title('Clean 1  short')
-subplot(512)
-plot(timeaxis,clean2s);
-title('Clean 2  short')
-subplot(513)
-plot(timeaxis,babbles);
-title('Babble  short')
-subplot(514)
-plot(timeaxis,nonstats);
-title('Artificial non static noise short')
-subplot(515)
-plot(timeaxis,shapeds);
-title('Speech shaped noise short')
+% subplot(511)
+% plot(timeaxis,clean1s);
+% title('Clean 1  short')
+% subplot(512)
+% plot(timeaxis,clean2s);
+% title('Clean 2  short')
+% subplot(513)
+% plot(timeaxis,babbles);
+% title('Babble  short')
+% subplot(514)
+% plot(timeaxis,nonstats);
+% title('Artificial non static noise short')
+% subplot(515)
+% plot(timeaxis,shapeds);
+% title('Speech shaped noise short')
 end
 
