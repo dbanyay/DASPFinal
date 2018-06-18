410ba7e98dcde354550a1191a03f04ca7c333ae2
qshtykoo <35166497+qshtykoo@users.noreply.github.com>
2018-06-18 17:02:06 +0200

    implemented modified SNR for original input

--- main.m
+++ main.m
@@ -4,7 +4,7 @@ clear all
 
 %% Read audio files
 
-[Fs,clean1s,clean2s,babbles,nonstats,shapeds,mixed1a,mixed1b] = readAudioFiles();
+[Fs,clean1s,clean2s,babbles,nonstats,shapeds,mixed1a,mixed1b,mixed1c] = readAudioFiles();
 
 %% Framing
 
@@ -14,45 +14,100 @@ overlap = 0.5; % for hanning window 50% is appropriate
 
 %input = babbles;
 %input = clean1s;
-%input = mixed1a;
-input = mixed1b;
+% input = mixed1a;
+% input = mixed1b;
+input = mixed1c;
 %input = ones(size(babbles));
 inputSize = size(input);
 
+framesTime_n  =windowing(babbles, windowSize, overlap);
+framesTime_c = windowing(clean1s, windowSize,overlap);
 framesTime = windowing(input,windowSize,overlap);
 
 
 %% Apply transform
 framesFreq = fft(framesTime')';  % transpose needed because we have rows with the frames, fft applies for columns
+framesFreq_c = fft(framesTime_c')';
+framesFreq_n = fft(framesTime_n')';
 
-
+framesFreqSquared = (abs(framesFreq).^2);
+alpha = 0.85;
+smoothed_framesFreq = reduce_variance(framesFreqSquared, alpha);
 %% Noise PSD estimator
 
-k = 25;  % slide window size
+k = 80;  % slide window size
+PSD_Noise = noisePSD(smoothed_framesFreq,Fs,k);
 
-PSD_Noise = noisePSD(framesFreq,Fs,k);
+t = [1:321]./windowSize*Fs;
+figure;
+plot(sqrt(PSD_Noise(:,250)),'r');
+hold on
+plot(sqrt(smoothed_framesFreq(:,250)));
+% plot(abs(framesFreq(:,100)),'b');
+plot(abs(framesFreq(:,250)),'g');
+hold off
 
+%% Speech PSD estimator 
+P_yy = framesFreqSquared;
+P_nn = PSD_Noise;
+Hwiener = max((P_yy - P_nn)./P_yy,0.2);
+PSD_Speech = (Hwiener.*abs(framesFreq)).*exp(complex(0,angle(framesFreq)));
+% figure;
+% plot(abs(sqrt(PSD_Speech(:,1))));
+% hold on
+% plot(abs(sqrt(framesFreqSquared(:,1))));
+% hold off
 
-%% Speech PSD estimator
+figure;
+plot(abs(PSD_Speech(:,50)),'r');
+hold on
+plot(abs(framesFreq_c(:,50)),'b');
+plot(abs(framesFreq(:,50)), 'g');
+hold off
 
+% framesSpeech = sqrt(PSD_Speech).*windowSize;
+framesSpeech = PSD_Speech;
+
+figure;
+subplot(2,1,1)
+plot(abs(framesFreq(:,1)));
+subplot(2,1,2)
+plot(abs(framesSpeech(:,1)));
+
+% Hwiener = (framesFreq(200,:)-sqrt(PSD_Noise(5,:)))./framesFreq(200,:);
+% figure;
+% plot(abs(Hwiener));
+% figure;
+% plot(abs(Hwiener.*framesFreq(200,:)));
+% figure;
+% plot(abs(framesFreq(200,:)));
 %% Gain function
 
 %% Apply gain
 
-framesProcessedFreq = applyGain(framesFreq);
+framesProcessedFreq = applyGain(framesSpeech);
+framesProcessedFreq_o = applyGain(framesFreq);
 
 
 %% Inverse transform
 
-framesProcessedTime = ifft(framesProcessedFreq')';
+framesProcessedTime = ifft(framesProcessedFreq','symmetric')';
+framesProcessedTime_o = ifft(framesProcessedFreq_o')';
 
 %% Overlap add
 
 output = overlapAdd(framesProcessedTime,windowSize, overlap, inputSize);
 
+figure;
+% plot(input)
+% hold on
+% plot(output)
+% hold off
 subplot(211)
 plot(input);
 title('Input')
+ylim([-0.5 0.5])
 subplot(212)
 plot(output);
 title('Output')
+ylim([-0.5 0.5])
--- noisePSD.m
+++ noisePSD.m
@@ -1,26 +1,14 @@
-function PSD_Noise = noisePSD(framesFreq,Fs,k)
+function noise_PSD = noisePSD(framesFreqSmoothed, Fs,k)
 % noisePSD create sliding window, estimate noise floor
 
-
 %% Create sliding window for each frame, take Qmin
-alpha = 0.6; % smoothing parameter for recursive averaging noise estimation
-for i = 1:size(framesFreq,2)  % iterate througs all freq segments
-    index = 1;
-    cntr = 1;
-    while index+k < size(framesFreq,1)  % iterate through frames
-       if index == 1
-        %Q(cntr,1:k) = framesFreq(index:index+k-1,i);
-            P_y(cntr) = 1/k*sum(framesFreq(index:index+k-1,i)); % Bartlett estimate
-            psd_n(cntr) = P_y(cntr);  % assume that the first time-frame is noise only
-       else
-            P_y(cntr) = 1/k*sum(framesFreq(index:index+k-1,i));
-            psd_n(cntr) = alpha * psd_n(cntr-1) + (1-alpha) * P_y(cntr);
-       end
 
-        index = index + k;
-        cntr = cntr +1;
-    end
-    PSD_Noise(i,:) = psd_n;      
+for bin = 1:size(framesFreqSmoothed,2)  % iterate through all bins
+    for time = 1:size(framesFreqSmoothed,1)-k+1  % iterate through time     
+
+        noise_PSD(time:time+k-1,bin) = min(framesFreqSmoothed(time:time+k-1,bin));        
+        
+    end    
 end
 
 end
