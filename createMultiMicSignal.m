733570495011264f4e0b3711875efbdd7703920f
qshtykoo <35166497+qshtykoo@users.noreply.github.com>
2018-06-22 22:56:17 +0200

    small changes

--- createMultiMicSignal.m
+++ createMultiMicSignal.m
@@ -1,4 +1,4 @@
-function [mic1,mic2,t] = createMultiMicSignal(cleanSpeech, noise, SNR, d, alpha, c, Fs)
+function [mic1,mic_sigs] = createMultiMicSignal(cleanSpeech, noise, SNR, d, alpha, c, Fs)
 %createMultichannelSignal Create multimicrophone signal
 
 P_s = sum((abs(cleanSpeech).^2)./length(cleanSpeech));
@@ -7,25 +7,16 @@ P_n = sum((abs(noise).^2)./length(noise));
 SNR_o = 10*log10(P_s./P_n); %original SNR
 P_n_desired = P_s./(10^(SNR/10));
 
+noise_new = noise.*sqrt(P_n_desired/P_n);
 
-noise1 = noise(1:length(cleanSpeech)/2);
-noise2 = noise(length(cleanSpeech)/2:length(cleanSpeech)-1);
+mic1 = cleanSpeech; %source signal
 
-cleanSpeech1 = cleanSpeech(1:length(cleanSpeech)/2);
+[H_PW, h_IR_PW] = calculate_transfer_function_plane_wave(c, d, alpha, Fs);
+[H_PW, h_IR_PW_Noise] = calculate_transfer_function_plane_wave(c, d, 100, Fs);
+mic_sigs = fftfilt(h_IR_PW, mic1);
+mic_noise = fftfilt(h_IR_PW_Noise, noise_new);
 
-t = abs(round(cos(alpha)*d/c*Fs));
-
-cleanSpeech2 = zeros(1,length(cleanSpeech1));
-cleanSpeech2(t:length(cleanSpeech1)) = cleanSpeech1(1:length(cleanSpeech1)-t+1);  % add delay
-
-
-noise1_new = noise1.*sqrt(P_n_desired/P_n);
-noise2_new = noise2.*sqrt(P_n_desired/P_n);
-
-
-mic1 = cleanSpeech1 + noise1_new;
-mic2 = cleanSpeech2 + noise2_new;
-
+mic_sigs = mic_sigs + mic_noise;
 
 end
 
--- delayAndSum.m
+++ delayAndSum.m
@@ -1,22 +1,32 @@
-function Sk = delayAndSum(freq1,freq2,t,alpha,Fs,c)
+function W = delayAndSum(freq1,alpha,Fs,c,d)
 %delayAndSum 
 
+% for j = 1:size(freq1,2)
+%     
+%     
+%     omega_vect = (freq2(:,j).*2*pi); % convert to radians
+%     delay = omega_vect.*sin(deg2rad(alpha))./c;
+%     
+%     freq2_delayed(:,j) = freq2(:,j).*exp(i.*delay);
+%     
+%     Sk(:,j) = (freq1(:,j)+freq2_delayed(:,j))./2;
+% 
+% 
+% end    
+num_of_sensors = 2;
+freq_length = size(freq1,2);
 
-
-
-
-for j = 1:size(freq1,2)
-    
-    
-    omega_vect = (freq2(:,j).*2*pi); % convert to radians
-    delay = omega_vect.*deg2rad(alpha)./c;
-    
-    freq2_delayed(:,j) = freq2(:,j).*exp(i.*delay);
-    
-    Sk(:,j) = (freq1(:,j)+freq2_delayed(:,j))./2;
-
-
-end    
+% Filter Design
+W = zeros(num_of_sensors, freq_length); % H is a frequency dependent M X N matrix
+for m = 1:num_of_sensors % for each sensor
+    for freq_idx = 1:freq_length % for each angular frequency
+        F_c = freq_idx*Fs/freq_length;
+        angularfreq = 2*pi*F_c;
+        shift = (angularfreq*d./c)*sin(deg2rad(alpha));
+        W(m,freq_idx) = exp(-1i * (m-1) * shift);
+    end
+end
+W = W./num_of_sensors; % Normalizing the output
 
 
 end
