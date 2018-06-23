964e4797b81efe52899d952965941cc8328f05d9
qshtykoo <35166497+qshtykoo@users.noreply.github.com>
2018-06-23 23:33:31 +0200

    modified delayandsum

--- delayAndSum.m
+++ delayAndSum.m
@@ -1,19 +1,6 @@
-% function Sk = delayAndSum(freq1,freq2, alpha, c, d)
 function W = delayAndSum(freq1,alpha, Fs,c,d)
 %delayAndSum 
 
-% for j = 1:size(freq1,2)
-%     
-%     
-%     omega_vect = (freq2(:,j).*2*pi); % convert to radians
-%     delay = omega_vect.*d.*sin(deg2rad(alpha))./c;
-%     
-%     freq2_delayed(:,j) = freq2(:,j).*exp(-1i.*delay);
-%     
-%     Sk(:,j) = (freq1(:,j)+freq2_delayed(:,j))./2;
-% 
-% 
-% end    
 num_of_sensors = 2;
 freq_length = size(freq1,2);
 
